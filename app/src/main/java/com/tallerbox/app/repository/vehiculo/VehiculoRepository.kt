06e13ebead626221e3ab7a654c699f0a18a536be
Jose Chale <chalejosejuan@gmail.com>
2025-09-27 14:00:40 -0600

    feat(VehiculoScreen):se creo la vista para ver la lista de vehiculos, igual que el repo y viewmodel

---
+++ app/src/main/java/com/tallerbox/app/repository/vehiculo/VehiculoRepository.kt
@@ -0,0 +1,11 @@
+package com.tallerbox.app.repository.vehiculo
+
+import com.tallerbox.app.model.vehiculo.VehiculoDao
+import com.tallerbox.app.model.vehiculo.VehiculoEntity
+import kotlinx.coroutines.flow.Flow
+
+class VehiculoRepository (private val dao: VehiculoDao){
+    suspend fun insertar(vehiculo: VehiculoEntity) = dao.insertar(vehiculo)
+
+    fun obtenerTodosFlow(): Flow<List<VehiculoEntity>> = dao.obtenerTodosFlow()
+}
---
+++ app/src/main/java/com/tallerbox/app/viewmodel/vehiculo/VehiculoViewModel.kt
@@ -0,0 +1,24 @@
+package com.tallerbox.app.viewmodel.cliente
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tallerbox.app.model.cliente.ClienteEntity
+import com.tallerbox.app.repository.cliente.ClienteRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class ClienteViewModel(private val repo: ClienteRepository) : ViewModel() {
+
+    val clientes: StateFlow<List<ClienteEntity>> =
+        repo.obtenerTodosFlow()
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
+
+    fun insertarCliente(cliente: ClienteEntity, onComplete: (() -> Unit)? = null) {
+        viewModelScope.launch {
+            repo.insertar(cliente)
+            onComplete?.invoke()
+        }
+    }
+}
